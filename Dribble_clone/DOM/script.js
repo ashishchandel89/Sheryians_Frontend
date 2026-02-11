8f8ca87ebc8f36ad17c2f2fbf93cd393d1654344
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-11 18:01:32 +0530

    Final नाम-जप counter app finished

--- Dribble_clone/DOM/script.js
+++ Dribble_clone/DOM/script.js
@@ -1,15 +1,33 @@
-let count=document.querySelector("h1");
-let button1=document.querySelector("#btn1");
-let button2=document.querySelector("#btn2");
+let congratulation = document.querySelector("h3");
+let count = document.querySelector("h1");
+let button1 = document.querySelector("#btn1");
+let button2 = document.querySelector("#btn2");
 
-let counter=0;
-button1.addEventListener("click",function(){
+let counter = 0;
+let target = 108;
+
+button1.addEventListener("click", function () {
+
     counter++;
-    count.innerHTML=counter;
-})
+    count.innerHTML = counter;
 
-button2.addEventListener("click",function(){
+    // show message at target
+    if (counter === target) {
+        congratulation.innerHTML ="Congratulations! You've reached " + target + " clicks!";
+        congratulation.style.color = "white";
+        target += 108;   
+    }
+
+    // clear message on next click
+    if (counter === target - 108 + 1) {
+        congratulation.innerHTML = "";
+        congratulation.style.color = "black";
+    }
+});
+
+button2.addEventListener("click", function () {
+
     counter--;
-    count.innerHTML=counter;
-})
-
+    if (counter < 0) counter = 0;
+    count.innerHTML = counter;
+});
