0815e39e4ed9476faf2d5094a3aa0129c28c4fd5
ashishchandel89 <chandel078ashish@gmail.com>
2026-03-06 14:52:38 +0530

    Que-2

--- Dribble_clone/ADV-js/adv2-js/script.js
+++ Dribble_clone/ADV-js/adv2-js/script.js
@@ -1,15 +1,28 @@
-class User{
-    constructor(name,email){
-        this.name=name;
-        this.email=email;
-    }
-    login(){
-        return loggedin;
+// class User{
+//     constructor(name,email){
+//         this.name=name;
+//         this.email=email;
+//     }
+//     login(){
+//         return loggedin;
+//     }
+// }
+
+// let user1=new User("Ashish","chandel078ashish@gmail.com");
+// let user2=new User("Manish","chandel098manish@gmail.com");
+// let user3=new User("Aakahsh","chandel08aakash@gmail.com");
+// let user4=new User("Krish","chandel07krish@gmail.com");
+// let user5=new User("Kanishak","chandel98kanishak@gmail.com");
+
+
+let Product={
+    name:"Addidas",
+    price:3000,
+    discount: 500,
+    finalprice: function(){
+        return this.price-this.discount;
     }
 }
+console.log(Product.finalprice());
 
-let user1=new User("Ashish","chandel078ashish@gmail.com");
-let user2=new User("Manish","chandel098manish@gmail.com");
-let user3=new User("Aakahsh","chandel08aakash@gmail.com");
-let user4=new User("Krish","chandel07krish@gmail.com");
-let user5=new User("Kanishak","chandel98kanishak@gmail.com");
+
