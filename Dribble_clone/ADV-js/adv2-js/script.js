e067dd7f0273df3a31c4316ac7b334ad1168a80b
ashishchandel89 <chandel078ashish@gmail.com>
2026-03-06 14:39:34 +0530

    Que1

---
+++ Dribble_clone/ADV-js/adv2-js/script.js
@@ -0,0 +1,15 @@
+class User{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+    login(){
+        return loggedin;
+    }
+}
+
+let user1=new User("Ashish","chandel078ashish@gmail.com");
+let user2=new User("Manish","chandel098manish@gmail.com");
+let user3=new User("Aakahsh","chandel08aakash@gmail.com");
+let user4=new User("Krish","chandel07krish@gmail.com");
+let user5=new User("Kanishak","chandel98kanishak@gmail.com");
