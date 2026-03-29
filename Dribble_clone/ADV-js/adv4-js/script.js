5a2d8572fd1fdc48db7ee5903fb60039ab0dd997
ashishchandel89 <chandel078ashish@gmail.com>
2026-03-29 16:03:19 +0530

    async await

--- Dribble_clone/ADV-js/adv4-js/script.js
+++ Dribble_clone/ADV-js/adv4-js/script.js
@@ -1,19 +1,35 @@
-const prm=new Promise((resolve,reject)=>{
-    let num=Math.floor(Math.random()*10);
-    console.log(`Generating number....`);
-    setTimeout(()=>{
-        if(num%2==0){
-            resolve(num);
-        }
-        else{
-            reject(num);
-        }
-    },1000);
-});
-prm
-.then(num=>{
-    console.log(`The number ${num} is even`);
+// const prm=new Promise((resolve,reject)=>{
+//     let num=Math.floor(Math.random()*10);
+//     console.log(`Generating number....`);
+//     setTimeout(()=>{
+//         if(num%2==0){
+//             resolve(num);
+//         }
+//         else{
+//             reject(num);
+//         }
+//     },1000);
+// });
+// prm
+// .then(num=>{
+//     console.log(`The number ${num} is even`);
+// })
+// .catch(num=>{
+//        console.log(`The number ${num} is odd`);
+// });
+
+fetch(`https://randomuser.me/api/`)
+.then((raw_data)=>raw_data.json())
+.then((raw_data)=>{
+    console.log(raw_data.results[0].name.first);
 })
-.catch(num=>{
-       console.log(`The number ${num} is odd`);
-});
+
+
+async function abcd(){
+    const raw_data=await fetch(`https://randomuser.me/api/`);
+    let data=await raw_data.json();
+    console.log(data.results[0].name.first);
+}
+setTimeout(() => {
+abcd();
+}, (1000));
